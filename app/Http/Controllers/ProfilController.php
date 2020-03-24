a1f261050dc0757d9033cef426e8ae53161cbea1
rayane tahiri <rayanetahiri@MacBook-Pro-de-rayane.local>
2020-03-24 14:07:08 +0100

    affichage des données d'un user dans mon profil

---
+++ app/Http/Controllers/ProfilController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Profil;
+use Illuminate\Http\Request;
+
+class ProfilController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view("profil/index");
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Profil  $profil
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Profil $profil)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Profil  $profil
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Profil $profil)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Profil  $profil
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Profil $profil)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Profil  $profil
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Profil $profil)
+    {
+        //
+    }
+}
